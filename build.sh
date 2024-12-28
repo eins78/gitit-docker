41345e6517cacd1287bb2944b712b3bcbe9a554c
Max Albrecht <1@178.is>
2024-12-28 15:12:53 +0100

    build script: publish image

--- build.sh
+++ build.sh
@@ -3,20 +3,21 @@
 set -exu
 
 GITIT_VERSION=0.15.1.2
+DEBIAN_VERSION=bullseye
 
-# test local
-docker build \
-  --build-arg GITIT_VERSION=${GITIT_VERSION} \
-  -t "eins78/gitit:${GITIT_VERSION}" \
-  .
-
-## build and publish
-#
-# docker buildx create --use
-# docker buildx build \
-#   --platform linux/amd64,linux/arm64 \
+# # test local
+# docker build \
 #   --build-arg GITIT_VERSION=${GITIT_VERSION} \
 #   -t "eins78/gitit:${GITIT_VERSION}" \
-#   --push \
 #   .
+
+# build and publish
+docker buildx create --use
+docker buildx build \
+  --platform linux/amd64,linux/arm64 \
+  --build-arg GITIT_VERSION=${GITIT_VERSION} \
+  --build-arg DEBIAN_VERSION=${DEBIAN_VERSION} \
+  -t "eins78/gitit:${GITIT_VERSION}" \
+  --push \
+  .
 
